4cbceda390b5c32420096e913febf315a028ab85
Dmitry Gribanov <chychydo88@gmail.com>
2021-10-25 15:41:39 +0300

    don't work selector to deleteModal

--- src/main/resources/static/js/modals.js
+++ src/main/resources/static/js/modals.js
@@ -15,7 +15,32 @@ $(document).ready(function (){
 
     });
 
-    $(' .table .dBtn').on('click', function (event){
+    let deleteModal = document.getElementById('deleteModal')
+    deleteModal.addEventListener('show.bs.modal', function (event) {
+        let button = event.relatedTarget
+        let href = button.getAttribute('data-bs-whatever')
+        $.get(href, function (user) {
+            $('.deleteForm #id1').attr('readonly','readonly').val(user.id);
+            $('.deleteForm #username1').attr('readonly','readonly').val(user.name);
+            $('.deleteForm #password1').attr('readonly','readonly').val(user.password);
+            let newOption = new Option('role.role', '1');
+            $('.deleteForm #roles1').add(newOption);
+
+/*            user.roles.forEach(role => $('.deleteForm #roles1').attr('readonly','readonly').);
+            if($('.deleteForm #roles1').options.clear()) {
+                for (let i = 0; i < $('.deleteForm #roles1').options.length; i++) {
+                    $('.deleteForm #roles1').options[i] = null;
+                }
+            }
+            $('.deleteForm #roles1').options.clear();
+            user.roles.forEach(role => {
+                let newOption = new Option(role.role, '');
+                $('.deleteForm #roles1').add(newOption);
+            });*/
+        });
+    })
+
+/*    $(' .table .dBtn').on('click', function (event){
         event.preventDefault();
         let href = $(this).attr('href');
         $.get(href, function (user) {
@@ -27,5 +52,5 @@ $(document).ready(function (){
 
         $('.deleteForm #exampleModal').modal();
 
-    });
+    });*/
 });
